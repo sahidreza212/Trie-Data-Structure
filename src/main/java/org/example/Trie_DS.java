9527c8ec7f383935e8475476f94b547238d9f6b9
Sahid Reza <sahidreza976@gmail.com>
2026-07-27 09:58:43 +0530

    Add Node class

--- src/main/java/org/example/Trie_DS.java
+++ src/main/java/org/example/Trie_DS.java
@@ -1,4 +1,34 @@
 package org.example;
 
 public class Trie_DS {
+    public static class Node{
+        Node[] children;
+        boolean eow;
+
+        public Node() {
+            children = new Node[26];
+            for (int i = 0; i < 26; i++) {
+                children[i] = null;
+            }
+            eow = false;
+        }
+    }
+    public static Node root = new Node();
+  public static void insert(String word){
+      Node curr = root;
+      for(int i = 0;i<word.length();i++){
+          int idx = word.charAt(i)-'a';
+
+          if(curr.children[idx] == null){
+              curr.children[idx] = new Node();
+          }
+          if(i == word.length()-1){
+              curr.children[idx].eow = true;
+          }
+          curr = curr.children[idx];
+      }
+  }
+    public static void main(String[] args) {
+
+    }
 }
